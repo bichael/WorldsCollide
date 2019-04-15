51b252ff9a5c4ae861dfa3aa43ffbabff0a8f5aa
Aaron Valenti <aaronval47@gmail.com>
2019-04-14 20:20:12 -0500

    added ladders dont work yet and some scenary

---
+++ WorldsCollide/Assets/ladderfup1.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ladderfup1 : MonoBehaviour
+{
+	/*void OnTriggerEnter2D(Collision2D col){
+		if(col.gameObject.name == "Player"){
+			col.gameObject.transform.position = new Vector3(0,0,0);(-33.74502,1.0576,0)
+			GetComponent<CharacterController>().enabled = false;
+		}
+	}*/
+
+	private void LateUpdate(){
+		if(GetComponent<CharacterController> ().enabled == false){
+			GetComponent<CharacterController> ().enabled = true;
+		}
+	}
+
+}
