cc60d766a8231a31237d461de86fe5c80dbe0784
Aaron Valenti <aaronval47@gmail.com>
2019-04-15 16:12:43 -0500

    teleporting should work

---
+++ WorldsCollide/Assets/Scripts/Teleport.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Collider2D))]
+public class Teleport : MonoBehaviour
+{
+	public enum TriggerType {Enter, Exit};
+
+	[Tooltip ("The Transform to teleport to")]
+	[SerializeField] Transform teleportTo;
+
+	[Tooltip ("The filter Tag")]
+	[SerializeField] string tag = "Player";
+
+	[Tooltip ("Trigger Event to Teleport")]
+	[SerializeField] TriggerType type;
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (type != TriggerType.Enter)
+			return;
+
+		if (tag == string.Empty || other.CompareTag(tag))
+			other.transform.position = teleportTo.position;
+	}
+
+	void OnTriggerExit2D (Collider2D other)
+	{
+		if (type != TriggerType.Exit)
+			return;
+
+		if (tag == string.Empty || other.CompareTag(tag))
+			other.transform.position = teleportTo.position;
+	}
+}
