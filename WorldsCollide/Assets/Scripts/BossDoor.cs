95c2062bfb83bf79c3fbeed392d454de56c5400b
Aaron Valenti <aaronval47@gmail.com>
2019-04-15 20:02:10 -0500

    Added doors, keys and enemy spawns but the enemies are still aggressive

---
+++ WorldsCollide/Assets/Scripts/BossDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BossDoor : MonoBehaviour
+{
+	public GameObject door;
+
+
+	/*public void Awake()
+    {
+        occupants = new List<Collider2D>();
+    }*/
+
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.name == "Player"){
+			if(GameController.control.keys == 4){
+			door.SetActive(false);
+		}
+
+
+	}
+
+	/*public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (ZoneContinuesToBeOccupied.GetPersistentEventCount() != 0)
+            ZoneContinuesToBeOccupied.Invoke();
+    }*/
+
+}
+}
---
+++ WorldsCollide/Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public static GameController control;
+	public int keys = 0;
+
+
+
+	void Awake(){
+		if(control == null){
+			DontDestroyOnLoad(gameObject);
+			control = this;
+		}
+		else if(control != this){
+			Destroy(gameObject);
+		}
+	}
+}
