840761585168e62a2906455a46bb1e5b99dacceb
mbakken <mjb556@msstate.edu>
2019-04-26 22:42:59 -0500

    :moneybag: Added inventory mechanic for player.  Exists in UI, allows player to see which items they are holding at the bottom of the screen.  Player can pick up equipment using F.  Currently the 3 items are hardcoded (sword=J, staff=K, shield=L), but this implementation will expand to only allowing 1 ranged weapon and 1 equipment to be equipped at a time, and its usage automatically being switched to the right key. * Updated Player, replaced HUD, & added Inventory prefabs. * Added some new sprites, including enemies and inventory icons.  - There's currently a glitch with the shield animation where the shield doesn't go down, not sure what's going on with it.

---
+++ WorldsCollide/Assets/Scripts/Inventory/Staff.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staff : InventoryItemBase
+{
+    public Player player;
+    public override void OnUse()
+    {
+        base.OnUse();
+    }
+
+    public override void OnPickup()
+    {
+        base.OnPickup();
+        player.HasStaff = true;
+    }
+}
