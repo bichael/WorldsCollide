b3ad775b6e95799d9dd184e514b8e316161d0e28
Aaron Valenti <aaronval47@gmail.com>
2019-04-30 21:47:08 -0500

    Fixed prisoner thing

---
+++ WorldsCollide/Assets/prisoner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class prisoner : MonoBehaviour
+{
+	public GameObject key;
+	private GameObject keyUI;
+
+	void Awake ()
+	{
+		keyUI = GameObject.FindGameObjectWithTag("KeyUI");
+	}
+
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.name == "Player"){
+			//key.SetActive(false);
+			GameController.control.keys += 1;
+			AssertKeyUI();
+		}
+	}
+
+	void AssertKeyUI()
+	{
+		keyUI.transform.GetComponentInChildren<Text>().text = GameController.control.keys.ToString() + "/4";
+	}
+}
